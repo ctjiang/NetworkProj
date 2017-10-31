06e66b9acb2564f1c18fadb6cd8e2e97145b7898
ctjiang <ctjiang@gmail.com>
2017-11-01 01:27:45 +0800

    Add Models folder

---
+++ MVC_Razor/Models/MovieContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesMovie.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies {get; set;}
+        public DbSet<Schedule> Schedules {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().ToTable("Movie");
+            modelBuilder.Entity<Schedule>().ToTable("Schedule");
+        }
+    }
+}
