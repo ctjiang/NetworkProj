1193ac0780d01c692ea88bd69de34a2f1863edae
ctjiang <ctjiang@gmail.com>
2017-11-09 01:20:34 +0800

    change Movie table ReleaseDate display name to "Release Date"

--- MVC_Razor/Models/Movie.cs
+++ MVC_Razor/Models/Movie.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MVC_Razor.Models
 {
@@ -6,6 +7,9 @@ namespace MVC_Razor.Models
     {
         public int ID {get; set;}
         public string Title {get; set;}
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate {get; set;}
         public string Gere {get; set;}
         public decimal Price {get; set;}
