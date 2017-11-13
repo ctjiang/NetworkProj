7cde72a52b635698178d880ad76db3a2ecb0b890
ctjiang <ctjiang@gmail.com>
2017-11-14 01:23:00 +0800

    Add Searching

--- MVC_Razor/Pages/Movies/Index.cshtml.cs
+++ MVC_Razor/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MVC_Razor.Models;
 
@@ -19,10 +20,26 @@ namespace MVC_Razor.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; }
+        public SelectList Genres;
+        public string MovieGenre {get; set;}
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string searchString)
         {
-            Movie = await _context.Movies.ToListAsync();
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movies
+                                            orderby m.Gere
+                                            select m.Gere;
+
+            var movies = from m in _context.Movies
+                         select m;
+            
+            if(!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync(); //_context.Movies.ToListAsync();
         }
     }
 }
